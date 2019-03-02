701e3fcb5d1a9da04cd0eb8ce3933a40ff0a5865
Ernie Val <erndval@gmail.com>
2019-03-02 22:41:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ function setCart(c) {
 function addToCart(item) {
   var itemPrice = Math.floor(Math.random() * Math.floor(100))
   var cartObj
-  cartObj[${item}] = itemPrice
+  cartObj[item] = itemPrice
+  cart = []
   return `${item} has been added to your cart.`
 }
 
